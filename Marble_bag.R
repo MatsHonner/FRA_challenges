967221908e49051592edde51be454a9d72ecf8a9
Honner Mats <mats.honner@volvo.com>
2024-05-31 11:25:31 +0200

    Marble_bag bug fixed

--- Marble_bag.R
+++ Marble_bag.R
@@ -8,13 +8,16 @@ Bag <- setRefClass(
     initialize = function() {
       marbles <<- c(rep("white", 25), rep("black", 25))
     },
-    
+
     pick_two = function() {
       if (length(marbles) < 2) {
         stop("Not enough marbles to pick two.")
       }
       picked <- sample(marbles, 2)
-      marbles <<- setdiff(marbles, picked)
+      index_to_remove <- which(marbles == picked[1])[1]
+      marbles <<- marbles[-index_to_remove]
+      index_to_remove <- which(marbles == picked[2])[1]
+      marbles <<- marbles[-index_to_remove]
       return(picked)
     },
     
@@ -31,17 +34,19 @@ Bag <- setRefClass(
   )
 )
 
-# Create an instance of the Bag class
-bag <- Bag$new()
 
-# Example usage:
-# 1. Pick two marbles
-picked_marbles <- bag$pick_two()
-print(paste("Picked marbles:", paste(picked_marbles, collapse = ", ")))
 
-# 2. Put back one marble (e.g., putting back a white marble)
-bag$put_back("white")
-
-# 3. Peek into the bag to see the current counts of white and black marbles
-current_counts <- bag$peek()
-print(current_counts)
+# # Create an instance of the Bag class
+# bag <- Bag$new()
+# 
+# # Example usage:
+# # 1. Pick two marbles
+# picked_marbles <- bag$pick_two()
+# print(paste("Picked marbles:", paste(picked_marbles, collapse = ", ")))
+# 
+# # 2. Put back one marble (e.g., putting back a white marble)
+# bag$put_back("white")
+# 
+# # 3. Peek into the bag to see the current counts of white and black marbles
+# current_counts <- bag$peek()
+# print(current_counts)
